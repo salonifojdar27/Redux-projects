68086335e606158ad11bf5257c755f30ffcb5f04
salonifojdar27 <salonifojdar27@gmail.com>
2025-08-19 22:51:18 +0530

    19/08/2025

---
+++ idcardgenerator/src/Components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import { useDispatch } from "react-redux";
+import { setSearchQuery } from "../features/idcardSlice";
+
+export default function SearchBar() {
+    const dispatch = useDispatch();
+
+    return (
+        <input
+            type="text"
+            placeholder="Search members..."
+            onChange={(e) => dispatch(setSearchQuery(e.target.value))}
+            className="border p-2 rounded w-full md:w-1/2 focus:outline-none focus:ring focus:ring-blue-400 mt-8 ml-55"
+        />
+    );
+}
