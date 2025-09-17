11b5475173d4ef76f143102860f61feacbc52fee
salonifojdar27 <salonifojdar27@gmail.com>
2025-09-17 11:48:27 +0530

    17/09/2025

---
+++ chatapp/src/Firebase/Firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAM6HK0mTMcUor6wsiP4LevagXpORxQmDE",
+    authDomain: "chatapp-clone-b2629.firebaseapp.com",
+    projectId: "chatapp-clone-b2629",
+    storageBucket: "chatapp-clone-b2629.firebasestorage.app",
+    messagingSenderId: "985435764792",
+    appId: "1:985435764792:web:70befe56dc86fb32808ecd",
+    measurementId: "G-H7W87JLR8Z"
+};
+
+const app = initializeApp(firebaseConfig);
